ce05169c0441f2828aacfce5ff72288e3c138d04
Margaux <margaux.aclo1941@gmail.com>
2020-02-09 19:10:37 +0100

    basic commit

--- controllers/fonction.php
+++ controllers/fonction.php
@@ -36,15 +36,15 @@ function setUsersRole($admin, $id)
     * à chaque fois que le compteur est inférieur à l'argument de la fonction, génère un caractère random de la liste avec rand().
     * retourne la chaîne de caractère générer.s
 */
+
 function generateRandomString($length = 10) {
     $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $charactersLength = strlen($characters); // retourne la taille de la chaine
     $randomString = "";
     for ($i = 0; $i < $length; $i++) { // à chaque fois que le compteur est inférieur à l'argument, incrémente de 1
         $index = rand(0, $charactersLength - 1); // génère un caractère au hasard
-        $character = $characters[$index];
+        $character = $characters[$index];  // ajoute les valeurs générer aléatoirement
         $randomString .= $character; // concatêne chaque caractère
-
     }
     return $randomString; // retourne le mot de passe provisoire.
 }
